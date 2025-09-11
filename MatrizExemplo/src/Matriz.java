9a06d702c0b7f5d170e95ab97144c66b41785e67
Jhonatan Fermino Da Silva <jhonatanfs2018@gmail.com>
2025-09-11 20:35:25 -0300

    Atividades do CLeiton

---
+++ MatrizExemplo/src/Matriz.java
@@ -0,0 +1,32 @@
+public class Matriz {
+    public static void main(String[] args){
+
+        int[] vetor = new int[5];
+
+
+        //int[linha][coluna]
+        int[][] matriz = new int[2][3];
+
+        vetor[1] = 5;
+
+        matriz[1][1] = 0;
+
+        for (int i = 0; i < 5; i++){
+            System.out.println(vetor[i]);
+        }
+
+
+        //matriz
+        for (int i = 0; i < 2; i++){//linha
+
+            //linha0
+
+            for (int j = 0; j < 3; j++){//coluna
+                //coluna 0
+                //coluna 1
+                //coluna 2
+            }
+
+        }
+    }
+}
